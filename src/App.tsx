10f5abdba9d4f951771a15b7902cb68cabf8c42b
Asitha Navaratne <asithan476@gmail.com>
2023-01-15 23:21:03 +0530

    Fetch data and display on grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
+import { ColDef } from "ag-grid-community";
+
+import Athlete from "./types/Athlete";
+import GridComponent from "./components/GridComponent";
+
+const url = "https://www.ag-grid.com/example-assets/olympic-winners.json";
+
 function App() {
-  return <div className="App"></div>;
+  const [rowData, setRowData] = useState<Athlete[]>([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((result) => result.json())
+      .then((result) => setRowData(result));
+  }, []);
+
+  const colDefs: ColDef<Athlete>[] = [
+    {
+      field: "athlete",
+    },
+    { field: "age" },
+    { field: "country" },
+    { field: "year" },
+    { field: "date" },
+    { field: "sport" },
+    { field: "gold" },
+    { field: "silver" },
+    { field: "bronze" },
+    { field: "total" },
+  ];
+
+  return (
+    <div className="App">
+      <GridComponent rowData={rowData} colDefs={colDefs} />
+    </div>
+  );
 }
 
 export default App;
