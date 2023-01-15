b25fd367c83f3ab76788f68e999686a24438d6af
Asitha Navaratne <asithan476@gmail.com>
2023-01-15 23:10:23 +0530

    AG Grid generic grid component created

--- src/components/GridComponent.tsx
+++ src/components/GridComponent.tsx
@@ -1,3 +1,38 @@
-const GridComponent = () => {};
+import { useMemo, useRef } from "react";
+
+import { AgGridReact } from "ag-grid-react";
+import { ColDef } from "ag-grid-community";
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine";
+
+import GridComponentProps from "../types/GridComponentProps";
+
+const GridComponent = <T extends {}>({
+  rowData,
+  colDefs,
+}: GridComponentProps<T>) => {
+  const gridRef = useRef<AgGridReact<T>>(null);
+
+  const defaultColDefs: ColDef<T> = useMemo(
+    () => ({
+      sortable: true,
+      filter: true,
+    }),
+    []
+  );
+
+  return (
+    <div className="ag-theme-alpine" style={{ height: 500 }}>
+      <AgGridReact<T>
+        ref={gridRef}
+        rowData={rowData}
+        columnDefs={colDefs}
+        defaultColDef={defaultColDefs}
+        animateRows={true}
+      />
+    </div>
+  );
+};
 
 export default GridComponent;
